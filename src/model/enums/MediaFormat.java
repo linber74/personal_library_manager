7ac0d0112cda89f0430ec16b4f9ae4bea045fb28
Linda Bergin <linber24@gmail.com>
2026-05-20 17:43:08 +0200

    Made better exception handling in MediaFormat

--- src/model/enums/MediaFormat.java
+++ src/model/enums/MediaFormat.java
@@ -1,5 +1,7 @@
 package model.enums;
 
+import java.util.logging.Logger;
+
 public enum MediaFormat {
 
     DIGITAL ("Digitalt"),
@@ -21,12 +23,15 @@ public enum MediaFormat {
         return format;
     }
 
+    private static final Logger LOGGER = Logger.getLogger(BookFormat.class.getName());
+
     public static MediaFormat fromString (String text){
         for (MediaFormat mediaFormat :  MediaFormat.values() ) {
             if (mediaFormat.format.equalsIgnoreCase(text)) {
                 return mediaFormat;
             }
         }
+        LOGGER.warning("Format " + text + " not supported");
         throw new IllegalArgumentException("Format " + text + " not supported");
     }
 }
